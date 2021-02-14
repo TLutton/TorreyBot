d128597a5ff64721f4f8e2e76d6071dd625608fb
MaxLutton <maxlutton@ucla.edu>
2021-02-13 16:17:11 -0800

    Move all requests to a single session

--- TorreyBot.py
+++ TorreyBot.py
@@ -2,32 +2,72 @@ import logging
 import argparse
 import time
 import getpass
+import typing
 import requests
+import json
+import smtplib
+import ssl
 
-login_url = "https://foreupsoftware.com/index.php/api/booking/users/login"
-login_form = {
+PAGE_URL = "https://foreupsoftware.com/index.php/booking/index/19347#/"
+LOGIN_URL = "https://foreupsoftware.com/index.php/api/booking/users/login"
+API_URL = "https://foreupsoftware.com/index.php/api/booking/"
+LOGIN_FORM = {
     "username": "",
     "password": "",
     "booking_class_id": "",
     "api_key": "no_limits",
     "course_id": 19347,
 }
-login_headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+LOGIN_HEADERS = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+emails = ["maxlutt55@gmail.com"]
+
+def get_times(username: str, password: str, date: str, players: int) -> typing.Optional[typing.Dict]:
+    with requests.Session() as sesh:
+        init_page = sesh.get(PAGE_URL)
+        cookies = init_page.cookies
+        login_form = LOGIN_FORM.copy()
+        login_form["username"] = username
+        login_form["password"] = password
+        logging.info("Attempting to login.")
+        login = sesh.post(LOGIN_URL, login_form, headers=LOGIN_HEADERS, cookies=cookies)
+        logging.debug(str(login.content))
+        if login.status_code != 200:
+            logging.error(f"Login failed. Error: {login.status_code}")
+            return None
+        times_url = (
+            API_URL + 
+            f"times?time=all&date={date}&holes=18&players={players}"
+            "&booking_class=888&schedule_id=1487&schedule_ids%5B%5D=0&schedule_ids%5B%5D=1468"
+            "&schedule_ids%5B%5D=1487&specials_only=0&api_key=no_limits"
+        )
+        logging.info(f"Getting tee times for {date}")
+        tee_times = sesh.get(times_url, cookies=login.cookies)
+        logging.debug(str(tee_times.content))
+        if tee_times.status_code != 200:
+            logging.error(f"Failed to get tee times. Error: {tee_times.status_code}")
+            return None
+        return str(tee_times.content)
+        
 
 
-def login(username, password):
-    login_form["username"] = username
-    login_form["password"] = password
-    logging.debug("Attempting to login.")
-    response = requests.post(login_url, login_form, headers=login_headers)
-    if response.status_code == requests.codes.ok:
-        logging.debug("Log in successful.")
-        logging.debug(response.content)
-        return True
-    else:
-        logging.debug("Log in failed. Error code: {}".format(response.status_code))
-        logging.debug(response.content)
-        return False
+def send_notification(times_data):
+    message = """Subject: Tee Times Found
+
+    
+    """
+    for entry in times_data:
+        players = entry.get("available_spots")
+        tee_time = entry.get("time")
+        message += f"Found time for {players} players at {tee_time}\n"
+    port = 465
+    username = input("Enter username: ")
+    password = getpass.getpass(prompt="Password: ", stream=None)
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        logging.info(f"Sending email {message}")
+        server.login(username, password)
+        server.sendmail(username, emails, message)
 
 
 def main():
@@ -49,7 +89,17 @@ def main():
         ],
     )
 
-    login(args.username, user_password)
+    query_date = "02-14-2021"
+    players = 2
+    
+    times = get_times(args.username, user_password, query_date, players)
+    if not times:
+        logging.error("Failed to get tee times.")
+        return 1
+    
+    
+    
+    # send_notification(times)
 
 
 if __name__ == "__main__":
